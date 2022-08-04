701b1868f1388b88eb7a313cb35764a26518b7a8
Adin Ashby <47254289+adinashby@users.noreply.github.com>
2022-08-04 15:14:58 -0400

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String input = scanner.nextLine();
+		 
+		/*
+		 * Add your code below this line
+		 */
+		
+		
+		/*
+		 * Add your code above this line
+		 */
+		
+		scanner.close();
+
+	}
+
+}
